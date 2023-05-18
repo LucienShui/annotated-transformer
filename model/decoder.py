db287b669a271d81ccbd717bcfa126f08caae446
Lucien <lucien@lucien.ink>
2023-05-18 22:15:13 +0800

    stash code

--- model/decoder.py
+++ model/decoder.py
@@ -1,11 +1,11 @@
 import torch
+from torch import Tensor
 from .attention import MultiHeadAttention, PositionWiseFeedForward
 from .util import ResidualConnect
-from typing import Tuple
 from copy import deepcopy
 
 
-class Encoder(torch.nn.Module):
+class Decoder(torch.nn.Module):
     def __init__(self, hidden_size: int, big_hidden_size: int, head_count: int, dropout_rate: float):
         super().__init__()
         self.hidden_size: int = hidden_size
@@ -14,13 +14,12 @@ class Encoder(torch.nn.Module):
         self.dropout_rate: float = dropout_rate
 
         self.attention = MultiHeadAttention(self.hidden_size, self.head_count, self.dropout_rate)
-        self.residual_attention = ResidualConnect(self.hidden_size, self.dropout_rate)
-        self.residual_feed_forward = deepcopy(self.residual_attention)
+        self.memory_attention = deepcopy(self.attention)
         self.feed_forward = PositionWiseFeedForward(self.hidden_size, self.big_hidden_size, self.dropout_rate)
 
-    def forward(self, x: torch.Tensor, mask: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
-        x = self.residual_attention(x, lambda _: self.attention(_, _, _, mask))
-        return self.residual_feed_forward(x, self.feed_forward), mask
+        self.attention_residual = ResidualConnect(self.hidden_size, self.dropout_rate)
+        self.memory_attention_residual = deepcopy(self.attention_residual)
+        self.feed_forward_residual = deepcopy(self.attention_residual)
 
-
-
+    def forward(self, x: Tensor, memory: Tensor, mask: Tensor):
+        pass
